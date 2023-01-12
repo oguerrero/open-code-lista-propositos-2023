659bad42c876fbecc7caa2790b9ac345a73ac470
oguerrero <oscarguerrero242@gmail.com>
2023-01-12 08:50:53 -0600

    add save to firebase

--- pages/components/ResolutionActions.js
+++ pages/components/ResolutionActions.js
@@ -1,25 +1,38 @@
-import { useState } from "react"
+// REACT
+import { useState } from 'react'
+
+// FIRESTORE
+import { app, firestore } from '../../firebaseConfig'
+import { collection, addDoc } from 'firebase/firestore'
 
 export default function ResolutionActions () {
-
   const [isInputVisible, setIsInputVisible] = useState(false)
   const [resolutionText, setResolutionText] = useState('')
 
+  const dbInstance = collection(firestore, 'resolutions')
+
   const inputToogle = () => setIsInputVisible(!isInputVisible)
 
+  const saveResolution = () => addDoc(dbInstance, { resolutionText: resolutionText })
+  
   return (
     <>
       <div className=''>
-        <button className='h-8 font-semibold text-white border-2 border-black cursor-pointer w-60 bg-violet-700 rounded-2xl'
+        <button
+          className='h-8 font-semibold text-white cursor-pointer w-60 bg-violet-700 rounded-2xl'
           onClick={ inputToogle }>
           Add New Year&apos;s resolution
         </button>
       </div>
 
       { isInputVisible && (
-        <div>
-          <input className="w-60 h-8 outline-none rounded-md border-gray-800 mx-1" placeholder='Enter the resolution...'
-            onChange={ (e) => setResolutionText(e.target.value) } />
+        <div className='flex flex-col gap-2'>
+          <input
+            className='w-60 h-8 mt-2 outline-none rounded-md border-2 border-gray-400 px-4 py-2'
+            placeholder='Enter the resolution...'
+            onChange={ (e) => setResolutionText(e.target.value) }
+          />
+          <button className='w-60 h-8 cursor-pointer bg-rose-800 text-white' onClick={ saveResolution }>Save</button>
         </div>
       ) }
     </>
