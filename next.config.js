a480dfb77d3df1e7ba0913309d507adf7d98520b
oguerrero <oscarguerrero242@gmail.com>
2023-01-12 18:32:31 -0600

    change next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+  }
 }
 
 module.exports = nextConfig
