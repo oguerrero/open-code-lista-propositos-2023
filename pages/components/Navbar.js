9e829a10456845078fd0d2f4155bcee0e0bb5f70
oguerrero <oscarguerrero242@gmail.com>
2023-01-12 18:26:10 -0600

    save user

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,13 +1,12 @@
 import { supabase } from "../../supabaseConfig"
 
 export default function Navbar() {
+  let user = ''
   const handleLogin = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: 'github',
-      options: {
-        redirectTo: 'https://example.com/welcome'
-      }
+      provider: 'github'
     })
+    user = data
   }
 
   const handleSignOut = async () => {
@@ -16,8 +15,8 @@ export default function Navbar() {
 
   return (
     <nav className='w-full h-8 px-8 py-2 flex flex-row'>
-      {data && <button onClick={handleSignOut}>Sign Out</button>}
-      {!data && (
+      {user && <button onClick={handleSignOut}>Sign Out</button>}
+      {!user && (
         <div className='flex flex-row gap-4 justify-end w-full'>
           <button
             onClick={handleLogin}
